a5b8b7c2f05184345cc9feb6e4aaed1dac8c2b59
Israel Umaña Sedó <isracr12@gmail.com>
2019-04-02 08:39:52 -0600

    x

--- src/scripts/sandbox/js-lang-basics.js
+++ src/scripts/sandbox/js-lang-basics.js
@@ -1,4 +1,5 @@
 import './js-lang-basics-header';
+import { isDeepStrictEqual } from 'util';
 
 
 /***********************************************************************************
@@ -321,7 +322,6 @@ GOOD LUCK 😀
 /* 
 John and Mike both play basketball in different teams. In the latest 3 games, John's 
 team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
-
 1. Calculate the average score for each team
 2. Decide which teams wins in average (highest average score), and print the winner to 
    the console. Also include the average score in the output.
@@ -508,13 +508,9 @@ Functions Statements and Expressions
 John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
 To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of 
 the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
-
 In the end, John would like to have 2 arrays:
-
 1) Containing all three tips (one for each bill)
-
 2) Containing all three final paid amounts (bill + tip).
-
 (NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
 GOOD LUCK 😀
 */
@@ -590,4 +586,80 @@ GOOD LUCK 😀
 /***********************************************************************************
     Objects and Methods
 ************************************************************************************/
-console.log("%cObjects and Methods:", 'color: #FF7433; font-weight: bolder');
+// console.log("%cObjects and Methods:", 'color: #FF7433; font-weight: bolder');
+
+// var israel = {
+//     name: 'Israel',
+//     lastName: 'Umaña Sedó',
+//     birthYear: 1989,
+//     familyMembers: ['Maureem', 'Mima', 'Sebastián', 'Gabriel', 'Isabel'],
+//     job: 'Web Content Developer', 
+//     isMarried: false, 
+//     calcAge: function () { // Function expresion
+//         this.age = 2019 - this.birthYear;
+//     }
+// };
+// israel.calcAge();
+// console.log(israel);
+
+
+
+
+
+/*---------------------------------------------------------------------------------
+    Challenge Three
+----------------------------------------------------------------------------------*/
+console.log("%cChallenge 3:", 'color: #FF7433; font-weight: bolder');
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. 
+Let's now implement the same functionality with objects and methods.
+
+1. For each of them, create an object with properties for their full name, mass, and height
+
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and 
+also return it from the method.
+
+3. In the end, log to the console who has the highest BMI, together with the full name and the 
+respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+*/
+
+var john = {
+    firstName:  'John',
+    lastName:   'Smith',
+    mass:       79,
+    height:     1.78,
+    calcBMI: function() {
+        this.BMI = Math.round(this.mass / (this.height * 2));
+    }
+}
+
+john.calcBMI();
+console.log(john.BMI);
+
+
+var mark = {
+    firstName:  'Mark',
+    lastName:   'Railey',
+    mass:       79,
+    height:     1.78,
+    calcBMI: function() {
+        this.BMI = Math.round(this.mass / (this.height * 2));
+    }
+}
+
+mark.calcBMI();
+console.log(mark.BMI);
+
+var j = john;
+var m = mark;
+
+if (j.BMI > m.BMI) {
+    console.log('John has the highest BMI with: ' + john.BMI);
+} else if (j.BMI == m.BMI) {
+    console.log('Both have ' + j.BMI + ' BMI');
+} else {
+    console.log('Mark has the highest BMI with: ' + mark.BMI);
+    
+}
