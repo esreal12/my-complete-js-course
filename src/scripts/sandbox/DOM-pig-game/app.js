6e2449fcacc82280e6739e44a661db3044db2a4a
Israel Umaña Sedó <isracr12@gmail.com>
2019-05-09 15:31:42 -0600

    Dom pig game code v2

--- src/scripts/sandbox/DOM-pig-game/app.js
+++ src/scripts/sandbox/DOM-pig-game/app.js
@@ -11,12 +11,14 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
-
-var x = document.querySelector('#score-0').textContent;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     // 1. random number
@@ -25,12 +27,44 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     // 2. display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.src = '/img/dice-' + dice + '.png';
+    diceDOM.src = '/src/img/dice-' + dice + '.png';
 
     // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player
+        nextPlayer();
+    }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+   
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]; 
 
+    // Check if player won the game
+    if (activePlayer )
+
+});
+
+function nextPlayer() {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
