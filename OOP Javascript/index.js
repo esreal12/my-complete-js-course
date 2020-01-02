2ebb8f84a3c5c6e6c2db9c229dbc9b3742ace4d1
A. Israel Umaña Sedó <isracr12@gmail.com>
2020-01-02 08:59:57 -0600

    01/02/19

--- OOP Javascript/index.js
+++ OOP Javascript/index.js
@@ -490,37 +490,48 @@
         }
     };
 
-    Question.prototype.checkAnswers = function(ans) {
+    Question.prototype.checkAnswers = function(ans, callback) {
+        let sc;
         if ( ans === this.correct) {
             console.log('Correct Answer!!!');
+            sc = callback(true);
         } else {
             console.log('Wrong answer, try again.');
+            sc = callback(false);
         }
+
+        this.displayScore(sc);
+    };
+
+    Question.prototype.displayScore = function (score) {
+        console.log('Your current score is: ' + score);
+        console.log('-------------------------------');   
     };
     
     let q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
     let q2 = new Question('What\'s the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
     let q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tedious']);
-    let questions = [q1, q2, q3];
-
+    
     function score () {
         let sc = 0;
         return function(correct){
             if (correct) {
                 sc++;
             }
-            return score;
+            return sc;
         };
-    }// Quedé en la función Score (Closure) Tengo que entender bien bien la razón de esto. 
-
+    }
+    
+    let keepScore = score();
     
     function nextQuestion () {
-        let n = Math.round(Math.random() * questions.length);        
+        let questions = [q1, q2, q3];
+        let n = Math.floor(Math.random() * questions.length);        
         questions[n].displayQuestion();
         let answer = prompt('Please select the correct answer (just type the letter). Or type exit to quit');
         
         if (answer !== 'exit') {
-            questions[n].checkAnswers(parseInt(answer));
+            questions[n].checkAnswers(parseInt(answer), keepScore);
             nextQuestion();
         };
     };
